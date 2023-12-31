a079505805b2d27435602854c014e2f092ea7601
Artem_PC <taftoos2@gmail.com>
2023-12-31 15:00:39 +0300

    hw1 update

--- hw1/src/main/java/ru/geekbrains/Domain/App.java
+++ hw1/src/main/java/ru/geekbrains/Domain/App.java
@@ -1,6 +1,7 @@
-package ru.geekbrains;
+package ru.geekbrains.Domain;
 
 import com.google.gson.Gson;
+import ru.geekbrains.Person;
 
 
 public final class App {
@@ -8,6 +9,10 @@ public final class App {
     public static void main(String[] args) {
 
         Person person1 = new Person("John", "Cohen", 30);
+        Person person2 = new Person("John", "Cohen", 30);
+        System.out.println(person1);
+        System.out.println(person2.equals(person1));
+
 
         // Инициализация библиотеки Gson
         Gson gson = new Gson();
