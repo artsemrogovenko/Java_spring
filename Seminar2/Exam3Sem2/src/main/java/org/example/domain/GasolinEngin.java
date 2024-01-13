840476faa087e695e67d108e96409de2ad432623
Artem_PC <taftoos2@gmail.com>
2024-01-13 22:47:22 +0300

    seminar основы spring

---
+++ Seminar2/Exam3Sem2/src/main/java/org/example/domain/GasolinEngin.java
@@ -0,0 +1,17 @@
+package org.example.domain;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class GasolinEngin implements iEngin {
+    public GasolinEngin() {
+        System.out.println("Создан экземпляр GasolinEngin");
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Запущен Бензин 44р.л");
+    }
+}
